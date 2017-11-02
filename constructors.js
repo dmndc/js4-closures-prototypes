3df61dd21c33f4656b390e7fff917564c0ce6b55
Dino Mandic <dinomandic@hotmail.com>
2017-11-02 15:00:20 -0500

    Complete all steps

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@
 
 // Code here
 
-
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
 
 
@@ -26,7 +29,7 @@ function Employee(name, email, hireDate) {
 }
 
 // Code here
-
+var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 
 
@@ -41,8 +44,17 @@ function Employee(name, email, hireDate) {
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+}
 
-
+Car.prototype.moveCar = function() {
+  this.move += 10;
+  return this.move;
+}
 
 
   //Function Invocations Here
